51f029a91b776463574d30a84472cdebdf44fbd8
a510102 <44679115+a510102@users.noreply.github.com>
2020-05-02 05:12:09 +0800

    Updates

---
+++ precache-manifest.cd34247b94b048b410c98b8ee752277f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd05b55338fbe1254b57e6a685a672d",
+    "url": "/AQI-react/index.html"
+  },
+  {
+    "revision": "9718bb0df35990f1f746",
+    "url": "/AQI-react/static/css/main.f92aa178.chunk.css"
+  },
+  {
+    "revision": "82743572abb75e16f652",
+    "url": "/AQI-react/static/js/2.d335b8aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AQI-react/static/js/2.d335b8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9718bb0df35990f1f746",
+    "url": "/AQI-react/static/js/main.b417a479.chunk.js"
+  },
+  {
+    "revision": "dc0c9c43050970be921e",
+    "url": "/AQI-react/static/js/runtime-main.74cb8e18.js"
+  }
+]);
